54c44fd33d20f1df44bbd791f625bfc358aaebe1
rjbhullar <rjbhullar28@gmail.com>
2022-06-03 15:29:14 -0400

    service worker setup

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('/service-worker.js', {scope: "/"})
         .then((registration) => {
             console.log('Success. Scope:', registration.scope);
         })
--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,16 @@
+
+self.addEventListener('install', (event) => {
+    console.log('install; ', event);
+    self.skipWaiting();
+});
+
+
+self.addEventListener('activate', (event) => {
+    console.log('activate;: ');
+    event.waitUntil(clients.claim());
+});
+
 self.addEventListener('fetch', () => {
+    console.log('fetch; ');
     return;
 });
