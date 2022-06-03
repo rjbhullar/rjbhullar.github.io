0808671136d972f31a3c691468fbe4a4d325936a
rjbhullar <rjbhullar28@gmail.com>
2022-06-03 14:11:27 -0400

    second commit

---
+++ app.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+            console.log('Success. Scope:', registration.scope);
+        })
+        .catch((error) => {
+            console.log('Failed. Error:', error);
+        });
+}
