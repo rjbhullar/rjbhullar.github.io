05ea9024d27a5fe467022a0c273c08eb81685bd0
rjbhullar <rjbhullar28@gmail.com>
2022-06-03 18:17:02 -0400

    cache setup done

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,35 @@
+const preCachedAssets = [
+    "/",
+    '/index.html',
+    '/main.css',
+    '/images/main.jpg',
+    '/icons/favicon-32x32.png'
+];
+
 
 self.addEventListener('install', (event) => {
-    console.log('install; ', event);
     self.skipWaiting();
+    event.waitUntil(caches.open("myCache").then((cache) => {
+        return cache.addAll(preCachedAssets)
+    }))
 });
 
 
 self.addEventListener('activate', (event) => {
-    console.log('activate;: ');
     event.waitUntil(clients.claim());
+    //remove old cache
+    event.waitUntil(caches.keys().then(function(names) {
+        console.log('names: ', names);
+        Promise.all(names.filter(name => name !== "myCache").map(name => caches.delete(name)))
+    }));
 });
 
-self.addEventListener('fetch', () => {
-    console.log('fetch; ');
-    return;
+//cache with network fallback
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                return response || fetch(event.request);
+            })
+    );
 });
